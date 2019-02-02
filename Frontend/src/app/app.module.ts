876cd45e175ecede5d235d645a6cdd98b0e617d6
Gabrielle Swanson <gabbyswanson@Gabrielles-MacBook-Pro.local>
2019-02-02 16:32:04 -0500

    Gabby 2-2

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubscribeFormComponent } from './subscribe-form/subscribe-form.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { FooterComponent } from './footer/footer.component';
+import { SubscribeListComponent } from './subscribe-list/subscribe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubscribeFormComponent,
+    SubscribeComponent,
+    FooterComponent,
+    SubscribeListComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule,
     AppRoutingModule
   ],
   providers: [],
