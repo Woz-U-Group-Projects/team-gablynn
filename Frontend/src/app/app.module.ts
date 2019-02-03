346c081829962fe67c1279dab1b048fdbfbdcadd
Gabrielle Swanson <gabbyswanson@Gabrielles-MacBook-Pro.local>
2019-02-02 19:50:47 -0500

    Karlynn fix

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubscribeFormComponent } from './subscribe-form/subscribe-form.component';
+import { SubscribeListComponent } from './subscribe-list/subscribe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubscribeFormComponent,
+    SubscribeListComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule,
     AppRoutingModule
   ],
   providers: [],
