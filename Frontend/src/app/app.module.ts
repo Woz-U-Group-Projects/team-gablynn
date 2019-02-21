1d1477ca9b710b1f0dea1d69b8b1a26b3a922d9c
Karlynn Headley <karlynnheadley22@gmail.com>
2019-02-20 22:23:37 -0800

    Popup bootstrap dependencies

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,7 +36,7 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule, FormsModule,
-    AppRoutingModule, HttpClientModule
+    AppRoutingModule, HttpClientModule, ModalModule.forRoot()
   ],
   providers: [PostsService, CommentsService],
   bootstrap: [AppComponent]
